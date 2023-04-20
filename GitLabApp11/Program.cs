20ad51a25762a7a37d7cc2c105494aa6a9891110
Hacob <jacob757f@gmail.com>
2023-04-20 13:16:57 +0300

    пример

--- GitLabApp11/Program.cs
+++ GitLabApp11/Program.cs
@@ -10,6 +10,14 @@ namespace GitLabApp11
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("введите значение парметра x:");
+            Console.WriteLine("введите значение парметра x:");
+            string s = Console.ReadLine();
+            double x = Convert.ToDouble(s);
+            double F = 2 * Math.Sin(x);
+            Console.WriteLine($"Значение F  = {F}.");
+            Console.ReadKey();
+
         }
     }
 }
